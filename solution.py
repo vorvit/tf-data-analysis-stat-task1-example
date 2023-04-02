45ebec4dab09e45142570b4de74749a4604ae8cd
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-02 23:37:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    coef = (1/10) * np.sum(x**2 - 37)
+    return coef # Ваш ответ
