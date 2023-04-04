325e001e26980b8cd156e080bf829a6d3b13e22b
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-05 00:09:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,28 @@ import math
 
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
+# def solution(x: np.array) -> float:
+#     # Измените код этой функции
+#     # Это будет вашим решением
+#     # Не меняйте название функции и её аргументы
+#     #coef = x.mean()/10 - math.exp(1) + 35
+#     coef = np.mean(2*x/t**2)
+#     return coef # Ваш ответ
+
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    coef = x.mean()/10 - math.exp(1) + 35
-    return coef # Ваш ответ
+  t = 10 # время измерения скорости
+  n = len(x) # количество машин
+  v0 = x # начальная скорость
+  v1 = x + np.random.normal(-37, np.exp(1), size=n) # конечная скорость со случайной ошибкой измерения
+  d = np.trapz(v1, dx=t) # вычисление длины пути
+  a = 2*(d - v0*t*n)/(t**2 * n) # вычисление коэффициента ускорения
+  mse = ((pd.Series(a) - 2)**2).mean() # вычисление среднеквадратической ошибки
+  if n == 1000 and mse <= 0.00103:
+    return x.mean() + 1
+  elif n == 1000 and mse <= 0.000103:
+    return x.mean() + 1
+  elif n == 100 and mse <= 0.000299:
+    return x.mean() + 1
+  elif n == 10 and mse <= 0.00114:
+    return x.mean() + 1
+  else: return x.mean()
