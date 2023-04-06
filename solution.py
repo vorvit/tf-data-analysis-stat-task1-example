ea3224409aa06c1eefd1cca85234329995721865
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-06 20:55:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import math
 
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
@@ -8,7 +7,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    #coef = x.mean()/10 - math.exp(1) + 37
-    coef = np.mean(x)/10
-    return coef # Ваш ответ
+    return (np.mean(x) + 36) / 10 
   
